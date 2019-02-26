a3a563f67b6dd163d1fe07782c757f80649fd8c3
Jack Foltz <jack@foltz.io>
2019-02-26 09:20:17 -0500

    Add keys to lists

--- src/calendar.js
+++ src/calendar.js
@@ -4,10 +4,10 @@ import './calendar.css';
 const percentage = n => n * 100 + '%';
 
 function Day(props) {
-    const items = props.items.map(item => {
+    const items = props.items.map((item, i) => {
         const pos = (item.start - props.start) / props.len;
         const height = (item.len / props.len);
-        return <div class="item" style={{top: percentage(pos), height: percentage(height)}}>{item.name}</div>;
+        return <div className="item" style={{top: percentage(pos), height: percentage(height)}} key={i}>{item.name}</div>;
     });
 
     return <div className="day">{items}</div>;
@@ -17,7 +17,7 @@ function Days(props) {
     return (
         <div className="days">
             {[...Array(5)].map((e, i) =>
-                 <Day start={props.start} len={props.len} items={props.items[i]} />)
+                 <Day start={props.start} len={props.len} items={props.items[i]} key={i} />)
             }
         </div>
     );
@@ -31,7 +31,7 @@ function Headers(props) {
     const dayNames = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
     return (
         <div className="headers">
-            {dayNames.map(d => <Header title={d} />)}
+            {dayNames.map(d => <Header title={d} key={d}/>)}
         </div>
     );
 }
@@ -44,7 +44,7 @@ function Times(props) {
     return (
         <div className="times">
             {[...Array(Math.floor(props.len / 60) + 1)].map((e, i) =>
-                <Time time={props.start + (i * 60)} pos={(i * 60) / props.len} />
+                <Time time={props.start + (i * 60)} pos={(i * 60) / props.len} key={i} />
             )}
         </div>
     );
@@ -58,7 +58,7 @@ function Separators(props) {
     return (
         <div className="separators">
             {[...Array(Math.floor(props.len / 60) + 1)].map((e, i) =>
-                <Separator pos={(i * 60) / props.len} />
+                <Separator pos={(i * 60) / props.len} key={i} />
             )}
         </div>
     );
